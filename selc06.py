ba35708eec51de0ee52202efcf6b0f52c7f4f8db
SungEunWoo <63888166+SungEunWoo@users.noreply.github.com>
2020-05-23 19:57:49 +0900

    Add files via upload

---
+++ selc06.py
@@ -0,0 +1,15 @@
+score = int(input("점수  입력 : "))
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+elif score >= 50:
+    print("E")
+else:
+    print("F")
+    
